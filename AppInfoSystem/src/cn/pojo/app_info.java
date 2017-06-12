6182cd09f940110f4b6f43439034cb9804e36579
la-nuit <936095513@qq.com>
2017-06-12 14:55:38 +0800

    有文件与你分享

---
+++ AppInfoSystem/src/cn/pojo/app_info.java
@@ -0,0 +1,177 @@
+package cn.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class app_info {
+	private int id;						//主键ID
+	private String softwareName;		//软件名称
+	private String APKName;				//APK名称
+	private String supportROM;			//支持ROM
+	private String interfaceLanguage;	//界面语言
+	private BigDecimal softwareSize;	//软件大小
+	private Date updateDate;			//更新日期
+	private int devId; 					//开发者ID
+	private String appInfo;				//应用简介
+	private int STATUS; 				//状态(来源于data_dctionary)
+	private Date onSaleDate;			//上架时间
+	private Date offSaleDate; 			//下架时间
+	private int flatformId; 			//所属平台(来源于data_dictionary)
+	private int categoryLevel3; 		//所属三级分类
+	private int downloads; 				//下载量
+	private int createdBy; 				//创建者
+	private Date creationDate; 			//创建时间
+	private int modifyBy; 				//更新者
+	private Date modifyDate;			//最新更新时间
+	private int categoryLevel1; 		//所属一级分类
+	private int categoryLevel2;			//所属二级分类
+	private String logoPicPath;			//LOGO图片url路径
+	private String logoLocPath;			//LOGO图片的服务器存储路径
+	private String versionId;			//最新的版本ID
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSoftwareName() {
+		return softwareName;
+	}
+	public void setSoftwareName(String softwareName) {
+		this.softwareName = softwareName;
+	}
+	public String getAPKName() {
+		return APKName;
+	}
+	public void setAPKName(String aPKName) {
+		APKName = aPKName;
+	}
+	public String getSupportROM() {
+		return supportROM;
+	}
+	public void setSupportROM(String supportROM) {
+		this.supportROM = supportROM;
+	}
+	public String getInterfaceLanguage() {
+		return interfaceLanguage;
+	}
+	public void setInterfaceLanguage(String interfaceLanguage) {
+		this.interfaceLanguage = interfaceLanguage;
+	}
+	public BigDecimal getSoftwareSize() {
+		return softwareSize;
+	}
+	public void setSoftwareSize(BigDecimal softwareSize) {
+		this.softwareSize = softwareSize;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public int getDevId() {
+		return devId;
+	}
+	public void setDevId(int devId) {
+		this.devId = devId;
+	}
+	public String getAppInfo() {
+		return appInfo;
+	}
+	public void setAppInfo(String appInfo) {
+		this.appInfo = appInfo;
+	}
+	public int getSTATUS() {
+		return STATUS;
+	}
+	public void setSTATUS(int sTATUS) {
+		STATUS = sTATUS;
+	}
+	public Date getOnSaleDate() {
+		return onSaleDate;
+	}
+	public void setOnSaleDate(Date onSaleDate) {
+		this.onSaleDate = onSaleDate;
+	}
+	public Date getOffSaleDate() {
+		return offSaleDate;
+	}
+	public void setOffSaleDate(Date offSaleDate) {
+		this.offSaleDate = offSaleDate;
+	}
+	public int getFlatformId() {
+		return flatformId;
+	}
+	public void setFlatformId(int flatformId) {
+		this.flatformId = flatformId;
+	}
+	public int getCategoryLevel3() {
+		return categoryLevel3;
+	}
+	public void setCategoryLevel3(int categoryLevel3) {
+		this.categoryLevel3 = categoryLevel3;
+	}
+	public int getDownloads() {
+		return downloads;
+	}
+	public void setDownloads(int downloads) {
+		this.downloads = downloads;
+	}
+	public int getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public int getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(int modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public int getCategoryLevel1() {
+		return categoryLevel1;
+	}
+	public void setCategoryLevel1(int categoryLevel1) {
+		this.categoryLevel1 = categoryLevel1;
+	}
+	public int getCategoryLevel2() {
+		return categoryLevel2;
+	}
+	public void setCategoryLevel2(int categoryLevel2) {
+		this.categoryLevel2 = categoryLevel2;
+	}
+	public String getLogoPicPath() {
+		return logoPicPath;
+	}
+	public void setLogoPicPath(String logoPicPath) {
+		this.logoPicPath = logoPicPath;
+	}
+	public String getLogoLocPath() {
+		return logoLocPath;
+	}
+	public void setLogoLocPath(String logoLocPath) {
+		this.logoLocPath = logoLocPath;
+	}
+	public String getVersionId() {
+		return versionId;
+	}
+	public void setVersionId(String versionId) {
+		this.versionId = versionId;
+	}
+	
+	
+}
