4e22e70fe8ee45c5fff2548c6ac21aef68bbeb13
han-jin <943567183@qq.com>
2017-06-12 17:52:08 +0800

    第一天

---
+++ AppInfoSystem/src/cn/service/App_infoService.java
@@ -0,0 +1,11 @@
+package cn.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.pojo.app_info;
+
+public interface App_infoService {
+	public List<app_info> getAllApp(String softwareName,int status,int flatformId);
+}
