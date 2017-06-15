6c131afbe158e70e33e557aa7272394d0dc0646f
han-jin <943567183@qq.com>
2017-06-15 14:27:28 +0800

    改错3

--- AppInfoSystem/WebRoot/statics/localjs/appinfoadd.js
+++ AppInfoSystem/WebRoot/statics/localjs/appinfoadd.js
@@ -101,11 +101,8 @@ $(function(){
 		//ajax后台验证--APKName是否已存在
 		$.ajax({
 			type:"GET",//请求类型
-<<<<<<< HEAD
+
 			url:"APKName",//请求的url
-=======
-			url:"/dev/APKName.html",//请求的url
->>>>>>> 5ec4319b840af750eda42ee330f74cf2aa44af0a
 			data:{APKName:$("#APKName").val()},//请求参数
 			dataType:"json",//ajax接口（请求url）返回的数据类型
 			success:function(data){//data：返回数据（json对象）
